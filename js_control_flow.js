8eea0e7e3772c1398630ac9b8fae9d00d1dcf630
Lakshika1485 <lakshika1485@gmail.com>
2024-03-05 12:26:39 +0530

    NEW CHECKDAY-WEEKDAY WEEKEND

--- js_control_flow.js
+++ js_control_flow.js
@@ -15,3 +15,24 @@ function checknumber(n)
   }
 }
 checknumber(5);
+
+document.write("<h4>WRITE A JAVASCRIPT PROGRAM THAT TAKES A DAY OF THE WEEK AS INPUT (using numbers 1 to 7, where 1 represnts sunday and 7 represents saturday) and prints whether it is a weekday or a weekend.....</h4>");
+
+
+function checkday(day)
+{
+    if(day>=1 && day<=5)
+    {
+        document.write('this is weekday');
+    }
+    else if(day>=6 && day<=7)
+    {
+        document.write('this is weekend');
+
+    }
+    else{
+        document.write('invalid day');
+
+    }
+}
+checkday(3);
